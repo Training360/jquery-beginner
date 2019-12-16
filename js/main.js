e38605eb89405b2848157f5fc9a649b537cc61c4
cherryApp <j.cserko@gmail.com>
2019-12-16 14:13:15 +0100

    Create video_08 branch

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 $(() => {
+    const img = $('img');
+    let isPlaying = false;
+
     $(window).on('keyup', (e) => {
         // Pause. (p)
         if (e.keyCode === 80) {
             console.log('paused');
         } else if (e.keyCode === 27) {
-            console.log('cancelled');
+            // Escape. (esc)
+            if (!isPlaying) {
+                isPlaying = true;
+                img.hide(1000)
+                    .show(1000, () => isPlaying = false);
+            }
         }
     });
 });
